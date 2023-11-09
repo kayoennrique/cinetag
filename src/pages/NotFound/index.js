881c18915bc0098a9fa2eaf2d95191ac0672d170
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2023-11-09 15:41:46 -0300

    Responsiveness and deployment

--- src/pages/NotFound/index.js
+++ src/pages/NotFound/index.js
@@ -1,11 +1,33 @@
 import styles from './NotFound.module.css';
+import erro404 from 'assets/erro_404.png'
 
 function NotFound() {
-    return(
-        <section className={styles.container}>
-            <h2>Ops!</h2>
-            <p>The content you are looking for was not found!</p>
-        </section>
+    return (
+        <>
+            <div className={styles.contentContainer}>
+                <span className={styles.text404}>404</span>
+
+                <h1 className={styles.title}>
+                    Oops! Page not found.
+                </h1>
+
+                <p className={styles.paragraph}>
+                    Are you sure this is what you were looking for?
+                </p>
+
+                <p className={styles.paragraph}>
+                    Wait a few moments and reload the page, or return to the home page.
+                </p>
+
+                <img
+                    className={styles.dogPicture}
+                    src={erro404}
+                    alt="Cachorro de óculos e vestido como humano"
+                />
+            </div>
+            <div className={styles.blankSpace}></div>
+        </>
+
     )
 }
 
